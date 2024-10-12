973d85a578bfcfe4d4fd704676f16a7311ac72f4
Michael Bella <bellamichael@web.de>
2024-10-12 17:48:06 +0200

    feat: request health status from hydra with hydra rust client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let mut ory_config = ory_hydra_client::apis::configuration::Configuration::new();
+    ory_config.base_path = "http://hydra:4444".to_string();
+
+    //requeet health status and print to console
+    let alive = ory_hydra_client::apis::metadata_api::is_alive(&ory_config)
+        .await
+        .unwrap();
+
+    print!("{:?}", alive);
 }
