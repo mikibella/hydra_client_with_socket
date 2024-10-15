6dc30d2544278468d10b2bb82be75b0d5c3dd1c8
Michael Bella <bellamichael@web.de>
2024-10-15 15:09:47 +0200

    test: add test to check if admin api is running

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 #[tokio::main]
-async fn main() {
-    let mut ory_config = ory_hydra_client::apis::configuration::Configuration::new();
-    ory_config.base_path = "http://hydra:4444".to_string();
+async fn main() {}
+#[cfg(test)]
+mod tests {
+    use hyper::{body::Body, client::conn, Request};
+    use reqwest::Client;
+    use tokio::net::UnixStream;
 
-    //requeet health status and print to console
-    let alive = ory_hydra_client::apis::metadata_api::is_alive(&ory_config)
+    #[tokio::test]
+    async fn test_if_admin_api_is_reachable() {
+        let mut ory_config = ory_hydra_client::apis::configuration::Configuration::new();
+        ory_config.base_path = "http://hydra:4445".to_string();
+
+        //requeet client form admin api
+        let response = ory_hydra_client::apis::o_auth2_api::list_o_auth2_clients(
+            &ory_config,
+            None,
+            None,
+            None,
+            None,
+        )
         .await
         .unwrap();
 
-    print!("{:?}", alive);
+        assert_eq!(response.len(), 0);
+    }
+    #[tokio::test]
+    async fn test_if_public_api_is_reachable_over_unix_socket() {
+        
+
 }
